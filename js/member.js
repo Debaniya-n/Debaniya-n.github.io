63410bcd4aea204f78ee5b631defe555793d665d
Debaa <pururualma@gmail.com>
2019-10-01 10:07:47 +0700

    DISQUS
    Added disqus

---
+++ js/member.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $('.carousel').slick({
+      autoplay: 1,
+      dots: true
+    });
+    $('.slider-for').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false,
+      fade: true,
+      asNavFor: '.slider-nav'
+    });
+    $('.slider-nav').slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      asNavFor: '.slider-for',
+      dots: true,
+      centerMode: true,
+      focusOnSelect: true
+    });
+  });
+  

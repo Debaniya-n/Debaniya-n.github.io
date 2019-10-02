cdeb100b56840888fd988a2c49118f10f8c6ef4a
Debaa <pururualma@gmail.com>
2019-10-02 20:34:26 +0700

    members
    yay :3c

--- js/member.js
+++ js/member.js
@@ -1,7 +1,21 @@
 $(document).ready(function(){
-    $('.members').slick({
-      autoplay: 1,
-      dots: true
-    });
+  $('.contents').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    swipe: false,
+    asNavFor: '.members'
+  });
+  $('.members').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.contents',
+    dots: false,
+    arrows: false,
+    centerMode: true,
+    focusOnSelect: true,
+  });
+      
   });
   
